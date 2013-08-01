c4d3cc03a4c9f56a1bc4f020808a5cf5f01ba6ed
Tom Chinery <tom.chinery@paymentcardsolutions.co.uk>
2013-08-01 16:11:36 +0100

    refactored ping script and added JS refresh and graph x y labels

--- lib/ping_script.rb
+++ lib/ping_script.rb
@@ -1,14 +1,16 @@
 require 'net/http'
 require 'uri'
 
-# Each instance of site, http request and store data in DB
-  Site.all.each do |site|
-    if site.on_off == true 
-      if ( ( Time.now.to_i - site.created_at.to_time.to_i ) / site.interval ).integer?
-        beginning_time = Time.now
-        res = Net::HTTP.get_response(URI(site.url))
-        end_time = Time.now
-        Request.create :url => "#{site.url}", :interval => "#{site.interval}", :code => "#{res.code}", :message => "#{res.message}", :response_time => "#{(end_time - beginning_time)}", :site_id => "#{site.id}" 
-      end
-    end
+def ping url
+  beginning_time = Time.now
+  res = Net::HTTP.get_response(URI(url))
+  Site.where(:url => url).each do |site|
+    Request.create :url => "#{site.url}", :interval => "#{site.interval}", :code => "#{res.code}", :message => "#{res.message}", :response_time => "#{(Time.now - beginning_time)}", :site_id => "#{site.id}" 
   end
+end
+
+# Each instance of site, http request and store data in DB
+  Site.where(:on_off => true).all.map(&:url).uniq.each do |url|
+    ping url
+  end
+
